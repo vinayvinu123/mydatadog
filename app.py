17c8bb5e9127ce68c04c0673dc2438796819bf65
vinays1 <vinays1@netapp.com>
2023-09-28 11:56:39 +0530

    final changes

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, vinay!'
+
+@app.route('/metrics', methods=['POST'])
+def get_metrics():
+    data = request.get_json()
+    print(data)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
